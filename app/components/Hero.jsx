9db827f7f5f9f08c6ec9743cdb6ca323a2051c11
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-09-14 14:44:46 -0500

    second commit

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
 export default function Hero() {
   return (
     <section style={poppins.style} className='relative'>
-        <div className='w-full mt-6 lg:mt-12 xl:mt-16 2xl:mt-40'>
+        <div className='w-full mt-6 lg:mt-12 xl:mt-16 2xl:mt-40 flex justify-between items-center flex-col lg:flex-row'>
             <div className='w-full lg:w-6/12'> 
                 <h1 className='font-extrabold text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl tracking-tighter text-[var(--title-color)] text-center lg:text-left'>
                     Hello, i'm <br />
@@ -23,11 +23,12 @@ export default function Hero() {
                     <button>Download my resume</button>
                 </div>
             </div>
-        </div>
-        <div className="absolute lg:right-48 2xl:right-80 top-[1%] lg:top-[15%] left-1/2 lg:left-auto -translate-x-[18%] lg:-translate-x-0 w-36 h-36 lg:w-72 lg:h-72 2xl:w-96 2xl:h-96 bg-gradient-to-b from-[#fa39ad] to-[#fe6c4c] blur-2xl lg:blur-[60px] 2xl:blur-[90px] -z-10 rounded-full flex items-end justify-center">
-            <div className='w-28 h-28 rounded-full bg-gradient-to-b from-[#fe6c4c] to-yellow-200'>
-
+            <div className='flex justify-center w-6/12'>
+                <img src="/3800_1_02-removebg-preview.png" className='w-64' alt="" />
             </div>
+        </div>
+        <div className="absolute top-[1%] lg:top-[20%] left-1/2 lg:left-1/2 -translate-x-[18%] lg:-translate-x-1/2 w-36 h-36 lg:w-[480px] lg:h-96  bg-gradient-to-b from-[#fa39ad] via-[#fe6c4c] to-blue-400 blur-2xl lg:blur-[123px] 2xl:blur-[165px] -z-10 rounded-full flex items-end justify-center">
+            
         </div>
     </section>
   )
